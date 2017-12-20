7e305ebb5eb7aece6c4a705c3d0d96a42ca50d72
Sabrina Nedjah <snedjah78@gmail.com>
2017-12-20 09:21:44 +0100

    laster modification

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,18 +1,59 @@
-$(document).ready(function() {
-    $(".title").lettering();
-  });
-  
-  
-  
-  $(document).ready(function() {
-    animation();
-  }, 1000);
-  
-  
-  function animation() {
-    var title1 = new TimelineMax();
-   
-    title1.staggerFromTo(".title span", 0.5, 
-    {ease: Back.easeOut.config(1.7), opacity: 0, bottom: -80},
-    {ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0}, 0.05);
-  }
+$animationHomePage = document.querySelector('.fontShadow')
+$splash = document.querySelector('.splash')
+// Delay for animation
+setTimeout(function(){ 
+    $animationHomePage.className = "fontShadow animationHomePage"
+}, 2000);
+
+
+var btnDiscover = document.querySelector('.discoverHome');
+var el = document.querySelector('.js-fade');
+
+btn.addEventListener('click', function(e){
+  el.classList.remove('is-paused');
+});
+
+
+
+/*
+window.scrollTo(0,document.body.scrollHeight);
+setTimeout(function(){ 
+    setTimeout(function(){ 
+        //faire un on click
+        $splash.className = "splash hidden displayNone"
+    }, 300);
+    
+    //faire un on click
+    $splash.className = "splash hidden"
+}, 1000);
+
+
+document.addEventListener('keydown', listenKeyboard)
+
+let slideVisible = -1;
+const slides = document.querySelectorAll('.slide')
+
+
+function listenKeyboard(event) {
+    event.preventDefault()
+    //slide du dessous
+
+   if(event.keyCode === 40){
+    slideVisible = slideVisible-1
+    for(var i = 0; i < slideVisible; i++){
+     slides[i].className = 'slide'
+    }
+    slides[slideVisible].className = 'slide translate100'
+
+   }
+
+   //slide dessus
+   else if(event.keyCode === 38) {
+        slideVisible = slideVisible+1
+       for(var i = 0; i< slideVisible; i++){
+        slides[i].className = 'slide translate100 translate200'
+       }
+
+       slides[slideVisible].className = 'slide translate100'
+   }
+*/
